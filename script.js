7ca6673413c1129d80abb547774d2cf3460fc3a4
wasilm10 <wasi.cd22@bmsce.ac.in>
2024-10-13 23:58:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+    // Add the JavaScript snippet here
+    window.addEventListener('load', function() {
+        document.body.classList.add('loaded');
+      });
+      function goBack() {
+        window.location.href = "index.html";
+    }
+   
